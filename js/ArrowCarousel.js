781c9ef7be6ae514441747f4c4c6a7501201e556
Siebe Geyskens <siebe-geyskens@hotmail.com>
2023-05-12 13:37:44 +0200

    refactor js - better naming

--- js/ArrowCarousel.js
+++ js/ArrowCarousel.js
@@ -2,72 +2,73 @@ export default class ArrowCarousel {
   constructor(itemsClassName) {
     this.items = Array.from(document.getElementsByClassName(itemsClassName));
     this.position = 0;
+    this.direction = null;
 
     this.init();
   }
 
-  togglePreviousItem() {
+  showPrevItem() {
     // hide previous item (card)
     this.items.at(this.position + 1).classList.toggle("card--hidden");
     // show next item (card)
     this.items.at(this.position).classList.toggle("card--hidden");
   }
 
-  toggleNextItem() {
+  showNextItem() {
     // hide previous item (card)
     this.items.at(this.position - 1).classList.toggle("card--hidden");
     // show next item (card)
     this.items.at(this.position).classList.toggle("card--hidden");
   }
 
-  // change position down
-  togglePrevious() {
+  // change internal position down
+  toPrevPosition() {
     if (this.position === 0) {
       // do nothing if carousel is already at the first item
       return;
     } else {
       this.position--;
-      this.togglePreviousItem();
+      this.showPrevItem();
     }
   }
 
-  // change position up
-  toggleNext() {
+  // change internal position up
+  toNextPosition() {
     if (this.position === this.items.length - 1) {
       // do nothing if carousel is already at the last item
       return;
     } else {
       this.position++;
-      this.toggleNextItem();
+      this.showNextItem();
     }
   }
 
   // Check to see if the carousel needs to go forward or backwards
-  togglePosition(e) {
+  // (find out if clicked in the left half or the right half of the card)
+  setDirection(e) {
     // use event.currentTarget which points to the element that you attached the listener and not it's children as it does when you click on the children
     // https://stackoverflow.com/questions/50149925/click-event-target-gives-element-or-its-child-not-parent-element
 
     // Get the bounding rectangle of target
     const itemRect = e.currentTarget.getBoundingClientRect();
 
-    // Mouse position relative to the currentTarget
+    // Mouse position relative to the currentTarget (carousel card that is clicked)
     const x = e.clientX - itemRect.left;
 
+
     if (x <= itemRect.width / 2) {
-      this.togglePrevious();
+      this.direction = "prev";
     } else {
-      this.toggleNext();
+      this.direction = "next";
     }
   }
 
-  toggle(e) {
-    this.togglePosition(e);
-  }
-
   init() {
+    // add event listeners
     this.items.forEach((item) =>
       item.addEventListener("click", (e) => {
-        this.toggle(e);
+        this.setDirection(e);
+        console.log(this.direction)
       })
     );
   }
