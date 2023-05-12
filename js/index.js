faec9aebf1df57a2b157ca8c255c2d5dc4d269ee
Siebe Geyskens <siebe-geyskens@hotmail.com>
2023-05-12 13:01:08 +0200

    better naming for carousels

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import ArrowCarousel from "./ArrowCarousel.js";
 import DotCarousel from "./DotCarousel.js";
 
 window.onload = () => {
-  const timeLineCarousel = new ArrowCarousel("timeline--carousel");
-  const skillsCarousel = new ArrowCarousel("skills--carousel");
-  const influencersCarousel = new DotCarousel("1nfluencers-carousel");
+  const timeLineCarousel = new ArrowCarousel("timeline-carousel-item");
+  const skillsCarousel = new ArrowCarousel("skills-carousel-item");
+  const influencersCarousel = new DotCarousel("1nfluencers-carousel-item");
 };
