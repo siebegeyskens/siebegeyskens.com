b1cdda29e9f08c87ddc75f2a7236579840bd68e5
Siebe Geyskens <siebe-geyskens@hotmail.com>
2023-05-13 12:24:07 +0200

    refactor carousels

--- js/ArrowCarousel.js
+++ js/ArrowCarousel.js
@@ -1,45 +1,60 @@
+// Simple carousel (with a direction and not finite):
+// 1. based on a classname add event listeners on all the class elements
+// 2. find out in which direction the carousel needs to go (clicked in left or right half of the element)
+// 3. set internal position based on direction (carousel is not infinite)
+// 4. based on internal position hide and show carousel elements
+
+
 export default class ArrowCarousel {
   constructor(itemsClassName) {
     this.items = Array.from(document.getElementsByClassName(itemsClassName));
-    this.position = 0;
     this.direction = null;
-
+    this.position = 0;
+    this.stopPostition = false;
+    this.length = this.items.length - 1;
     this.init();
   }
+  
+  // Show items based on position
+  showItems() {
 
-  showPrevItem() {
-    // hide previous item (card)
-    this.items.at(this.position + 1).classList.toggle("card--hidden");
-    // show next item (card)
-    this.items.at(this.position).classList.toggle("card--hidden");
-  }
-
-  showNextItem() {
-    // hide previous item (card)
-    this.items.at(this.position - 1).classList.toggle("card--hidden");
-    // show next item (card)
-    this.items.at(this.position).classList.toggle("card--hidden");
-  }
-
-  // change internal position down
-  toPrevPosition() {
-    if (this.position === 0) {
-      // do nothing if carousel is already at the first item
-      return;
-    } else {
-      this.position--;
-      this.showPrevItem();
+    // 1. HIDE THE PREVIOUS ITEM
+    if (this.direction === "prev"){
+      // hide previous item (card right of the one to show)
+      this.items.at(this.position + 1).classList.toggle("card--hidden");
+    } else if (this.direction === "next"){  
+      // hide previous item (card left of the one to show)
+      this.items.at(this.position - 1).classList.toggle("card--hidden");
     }
+
+    // 2. SHOW THE CURRENT ITEM
+    this.items.at(this.position).classList.toggle("card--hidden");
   }
 
-  // change internal position up
-  toNextPosition() {
-    if (this.position === this.items.length - 1) {
-      // do nothing if carousel is already at the last item
+  // Set internal position, based on direction
+  setPosition() {
+
+    // dont set position if at start or end of carousel
+    // and set a stop
+    if (this.position === 0 && this.direction === "prev" ) {
+      this.stopPostition = true;
       return;
-    } else {
-      this.position++;
-      this.showNextItem();
+    }
+    if (this.position === this.length && this.direction === "next") {
+      this.stopPostition = true;
+      return;
+    }
+
+    // reset stop
+    if (this.stopPosition = true) this.stopPostition = false;
+
+    // SET TO PREV POSITION
+    if (this.direction === "prev") {
+        this.position--;
+
+    // SET TO NEXT POSITION 
+    } else if (this.direction === "next") {
+        this.position++;
     }
   }
 
@@ -55,7 +70,8 @@ export default class ArrowCarousel {
     // Mouse position relative to the currentTarget (carousel card that is clicked)
     const x = e.clientX - itemRect.left;
 
-
+    // Set the direction in the state
+    // This will be used by further functions
     if (x <= itemRect.width / 2) {
       this.direction = "prev";
     } else {
@@ -63,12 +79,17 @@ export default class ArrowCarousel {
     }
   }
 
+  move(e) {
+    this.setDirection(e);
+    this.setPosition();
+    if (!this.stopPostition) this.showItems();
+  }
+
   init() {
     // add event listeners
     this.items.forEach((item) =>
       item.addEventListener("click", (e) => {
-        this.setDirection(e);
-        console.log(this.direction)
+        this.move(e);
       })
     );
   }
--- js/DotCarousel.js
+++ js/DotCarousel.js
@@ -1,12 +1,17 @@
+// Simple carousel (infinite and no direction)
+// 1. based on a classname add event listeners on all the class elements
+// 2. set internal position
+// 3. show items based on position
+
+
 export default class DotCarousel {
   constructor(itemsClassName) {
     this.items = Array.from(document.getElementsByClassName(itemsClassName));
     this.position = 0;
-
     this.init();
   }
 
-  togglePosition() {
+  setPosition() {
     if (this.position === this.items.length - 1) {
       this.position = 0;
     } else {
@@ -14,22 +19,23 @@ export default class DotCarousel {
     }
   }
 
-  toggleItems() {
+  showItems() {
     // hide previous item (card)
     this.items.at(this.position - 1).classList.toggle("card--hidden");
     // show next item (card)
     this.items.at(this.position).classList.toggle("card--hidden");
   }
 
-  toggle(e) {
-    this.togglePosition();
-    this.toggleItems();
+  move(e) {
+    this.setPosition();
+    this.showItems();
   }
 
   init() {
     this.items.forEach((item) =>
       item.addEventListener("click", (e) => {
-        this.toggle(e);
+        this.move(e);
+        console.log(this.position);
       })
     );
   }
