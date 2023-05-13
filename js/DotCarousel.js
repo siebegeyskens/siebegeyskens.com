a2c4d7c96d435f0bd5af97376f2bf67205c22638
Siebe Geyskens <siebe-geyskens@hotmail.com>
2023-05-13 15:09:29 +0200

    add project cards + autom. set project card carousel items

--- js/DotCarousel.js
+++ js/DotCarousel.js
@@ -1,7 +1,11 @@
 // Simple carousel (infinite and no direction)
-// 1. based on a classname add event listeners on all the class elements
-// 2. set internal position
-// 3. show items based on position
+
+// 1. MOVING THE CAROUSEL
+// 1.1 based on a classname add event listeners on all the class elements
+// 1.2 set internal position
+// 1.3 show items based on position
+// 2. HANDLING THE HEIGHTS 
+// 
 
 
 export default class DotCarousel {
@@ -10,6 +14,9 @@ export default class DotCarousel {
     this.position = 0;
     this.init();
   }
+
+  // Moving the carousel
+  // -------------------
 
   setPosition() {
     if (this.position === this.items.length - 1) {
@@ -35,8 +42,36 @@ export default class DotCarousel {
     this.items.forEach((item) =>
       item.addEventListener("click", (e) => {
         this.move(e);
-        console.log(this.position);
       })
     );
+
+    this.setItemsHeight();
+  }
+
+  // Handling the heights of the carousel items:
+  // set the height all all the items based on the first items height
+  // ------------------------------------------
+
+  setItemsHeight = () => {
+
+    // get height of the first item
+    let itemHeight;
+    // in case the first card is hidden then it doesn't have a height
+    // to fix this show it, get the hide, hide it.
+    if (this.items[0].classList.contains('card--hidden')) {
+      this.items[0].classList.toggle("card--hidden");
+      itemHeight = this.items[0].clientHeight;
+      this.items[0].classList.toggle("card--hidden");
+    } else {
+      itemHeight = this.items[0].clientHeight;
+    }
+
+    // set other card height first card height
+    // but not of the first one, so that whenever the user resizes the screen we can resize the other cards again
+    this.items.forEach((card, i)=> {
+      if (i !== 0) {
+        card.setAttribute(`style`,`height:${itemHeight}px`)
+      }
+    })
   }
 }
--- js/index.js
+++ js/index.js
@@ -2,7 +2,24 @@ import ArrowCarousel from "./ArrowCarousel.js";
 import DotCarousel from "./DotCarousel.js";
 
 window.onload = () => {
+
+  // initialize carousels
+  // ---------------------------------------------------------------
+  
   const timeLineCarousel = new ArrowCarousel("timeline-carousel-item");
   const skillsCarousel = new ArrowCarousel("skills-carousel-item");
-  const influencersCarousel = new DotCarousel("1nfluencers-carousel-item");
+  const wfnCarousel = new DotCarousel("wfn-carousel-item");
+  const infCarousel = new DotCarousel("inf-carousel-item");
+  const seshCarousel = new DotCarousel("sesh-carousel-item");
+  const aLiveCarousel = new DotCarousel("a-live-carousel-item");
+  
+  // user resizes: 
+  onresize = (event) => {
+    wfnCarousel.setItemsHeight();
+    infCarousel.setItemsHeight();
+    seshCarousel.setItemsHeight();
+    aLiveCarousel.setItemsHeight();
+  };
+
+  // ---------------------------------------------------------------
 };
