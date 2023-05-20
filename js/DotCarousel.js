49b0a4c0aad05c7fcf44c0fc6bfa10e31bc2d1fa
Siebe Geyskens <siebe.geyskens@student.ehb.be>
2023-05-20 17:09:13 +0200

    media queries + some minor stuff

--- js/DotCarousel.js
+++ js/DotCarousel.js
@@ -4,15 +4,18 @@
 // 1.1 based on a classname add event listeners on all the class elements
 // 1.2 set internal position
 // 1.3 show items based on position
-// 2. HANDLING THE HEIGHTS 
-// 
-
+// 2. HANDLING THE HEIGHTS:
+// 2.1 set the height all all the items based on the first items height on initialize
+// 2.2 same when user resizes
 
 export default class DotCarousel {
   constructor(itemsClassName) {
     this.items = Array.from(document.getElementsByClassName(itemsClassName));
     this.position = 0;
     this.init();
+
+    //
+    this.name = itemsClassName;
   }
 
   // Moving the carousel
@@ -38,27 +41,25 @@ export default class DotCarousel {
     this.showItems();
   }
 
-  init() {
-    this.items.forEach((item) =>
-      item.addEventListener("click", (e) => {
-        this.move(e);
-      })
-    );
-
-    this.setItemsHeight();
-  }
-
   // Handling the heights of the carousel items:
   // set the height all all the items based on the first items height
   // ------------------------------------------
 
-  setItemsHeight = () => {
+  // set the items hight to auto
+  resetItemsHeight = () => {
+    this.items.forEach((card, i) => {
+      if (i !== 0) {
+        card.setAttribute(`style`, `height:auto`);
+      }
+    });
+  };
 
+  setItemsHeight = () => {
     // get height of the first item
     let itemHeight;
     // in case the first card is hidden then it doesn't have a height
     // to fix this show it, get the hide, hide it.
-    if (this.items[0].classList.contains('card--hidden')) {
+    if (this.items[0].classList.contains("card--hidden")) {
       this.items[0].classList.toggle("card--hidden");
       itemHeight = this.items[0].clientHeight;
       this.items[0].classList.toggle("card--hidden");
@@ -68,10 +69,26 @@ export default class DotCarousel {
 
     // set other card height first card height
     // but not of the first one, so that whenever the user resizes the screen we can resize the other cards again
-    this.items.forEach((card, i)=> {
+    this.items.forEach((card, i) => {
       if (i !== 0) {
-        card.setAttribute(`style`,`height:${itemHeight}px`)
+        card.setAttribute(`style`, `height:${itemHeight}px`);
       }
-    })
+    });
+  };
+
+  init() {
+    this.items.forEach((item) =>
+      item.addEventListener("click", (e) => {
+        this.move(e);
+      })
+    );
+
+    if (window.screen.width < 768) {
+      this.setItemsHeight();
+    }
+
+    // onresize = (event) => {
+    //   this.setItemsHeight();
+    // };
   }
 }
--- js/index.js
+++ js/index.js
@@ -2,23 +2,43 @@ import ArrowCarousel from "./ArrowCarousel.js";
 import DotCarousel from "./DotCarousel.js";
 
 window.onload = () => {
-
   // initialize carousels
   // ---------------------------------------------------------------
-  
+
+  let lastWidth;
+
   const timeLineCarousel = new ArrowCarousel("timeline-carousel-item");
   const skillsCarousel = new ArrowCarousel("skills-carousel-item");
   const wfnCarousel = new DotCarousel("wfn-carousel-item");
   const infCarousel = new DotCarousel("inf-carousel-item");
   const seshCarousel = new DotCarousel("sesh-carousel-item");
   const aLiveCarousel = new DotCarousel("a-live-carousel-item");
-  
-  // user resizes: 
+
+  // user resizes:
   onresize = (event) => {
-    wfnCarousel.setItemsHeight();
-    infCarousel.setItemsHeight();
-    seshCarousel.setItemsHeight();
-    aLiveCarousel.setItemsHeight();
+    // set all the heights
+
+    console.log(lastWidth);
+
+    if (window.screen.width < 768) {
+      wfnCarousel.setItemsHeight();
+      infCarousel.setItemsHeight();
+      seshCarousel.setItemsHeight();
+      aLiveCarousel.setItemsHeight();
+    }
+
+    // if came from 767 then set the items height to that of the grid
+    // So that means if jumping from mobile to tablet set the project items height to auto
+    // So that the grid settings will define the hight of the items
+    if (lastWidth <= 767 && window.screen.width >= 768) {
+      wfnCarousel.resetItemsHeight();
+      infCarousel.resetItemsHeight();
+      seshCarousel.resetItemsHeight();
+      aLiveCarousel.resetItemsHeight();
+      console.log("reset!");
+    }
+
+    lastWidth = window.screen.width;
   };
 
   // ---------------------------------------------------------------
